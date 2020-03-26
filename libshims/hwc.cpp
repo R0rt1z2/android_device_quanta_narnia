784370befec74902b09d784b14413147deb57254
Roger Ortiz <39171306+R0rt1z2@users.noreply.github.com>
2020-03-26 13:07:29 +0100

    narnia: Fix hardware composer shim.

--- libshims/hwc.cpp
+++ libshims/hwc.cpp
@@ -1,1 +1,14 @@
-extern "C" void __umodsi3(void) {}
+#include <stdint.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+extern "C"
+{
+    /* Return the remainder of the unsigned division of a and b */
+    unsigned int __umodsi3(unsigned int a, unsigned int b) 
+    {
+        unsigned int rem;
+        rem = a % b;
+        return rem;
+    }
+}
