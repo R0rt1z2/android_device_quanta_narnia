810ac0bf105fd06e2ea8c081644e4aa0d07c030c
R0rt1z2 <rogerortizleal@gmail.com>
2020-03-29 22:57:16 +0200

    narnia: Fix apply-patches.sh
    Signed-off-by: R0rt1z2 <rogerortizleal@gmail.com>

--- patches/apply-patches.sh
+++ patches/apply-patches.sh
@@ -4,9 +4,9 @@ PATCHES_DIR="device/quanta/narnia/patches" # Change this with your device path!
 
 echo "Applying patches..."
 
-cd system/core/init
+cd system/core/init/
 
-git apply -v ../../$PATCHES_DIR/system/core/init/0001-MEDIATEK-MT8127-INIT.patch
+git apply -v ../../../$PATCHES_DIR/system/core/init/0001-MEDIATEK-MT8127-INIT.patch
 
 cd ../..
 
